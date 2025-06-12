a77e9f983c325a1e8c66da6812a402d33fb8bb75
Francesco Bottino <francesco.bottino96@gmail.com>
2025-06-12 23:33:32 +0200

    cleanup winner check

--- game-tictactoe-server/src/main/kotlin/com/francescobottino/thehubproject/games/tictactoe/usecase/utils.kt
+++ game-tictactoe-server/src/main/kotlin/com/francescobottino/thehubproject/games/tictactoe/usecase/utils.kt
@@ -3,14 +3,15 @@ package com.francescobottino.thehubproject.games.tictactoe.usecase
 import com.francescobottino.thehubproject.games.tictactoe.model.*
 import com.francescobottino.thehubproject.games.tictactoe.model.TicTacToeGameRoom.State
 
-
 fun TicTacToeGameState.getWinnerSign(): TicTacToePlayerSign? {
-    val groups = (0..2).map { r -> (0..2).map { c -> this[TicTacToeBoardCell(r, c)] } } +  //rows
-            (0..2).map { c -> (0..2).map { r -> this[TicTacToeBoardCell(r, c)] } } +       //cols
-            listOf(                                                                                    //diagonals
-                (0..2).map { i -> this[TicTacToeBoardCell(i, i)] },
-                (0..2).map { i -> this[TicTacToeBoardCell(i, 2 - i)] }
-            )
+    val rows = (0..2).map { r -> (0..2).map { c -> this[TicTacToeBoardCell(r, c)] } }
+    val cols = (0..2).map { c -> (0..2).map { r -> this[TicTacToeBoardCell(r, c)] } }
+    val diagonals = listOf(
+        (0..2).map { i -> this[TicTacToeBoardCell(i, i)] },
+        (0..2).map { i -> this[TicTacToeBoardCell(i, 2 - i)] }
+    )
+
+    val groups = rows + cols + diagonals
 
     return groups
         .firstOrNull { group -> group.none { it == null } && group.distinct().size == 1 } //group is full and all signs are the same sign
