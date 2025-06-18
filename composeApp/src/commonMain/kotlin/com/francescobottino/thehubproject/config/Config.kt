3653e240266aa30b518b2cae788f71377fe0c3f6
Francesco Bottino <francesco.bottino96@gmail.com>
2025-06-18 16:51:15 +0200

    Added better example to local.properties

--- composeApp/src/commonMain/kotlin/com/francescobottino/thehubproject/config/Config.kt
+++ composeApp/src/commonMain/kotlin/com/francescobottino/thehubproject/config/Config.kt
@@ -3,7 +3,7 @@ package com.francescobottino.thehubproject.config
 import com.francescobottino.thehubproject.ClientConfig
 
 object Config {
-    val isProduction: Boolean = ClientConfig.ENVIRONMENT?.contains("dev") != true
+    private val isProduction: Boolean = ClientConfig.ENVIRONMENT?.contains("dev") != true
     private const val PRODUCTION_ENDPOINT = "thehubproject-api.up.railway.app"
     private val serverEndpoint: String = if(isProduction) PRODUCTION_ENDPOINT else "${internalServerDebugIp(ClientConfig.DEV_SERVER_IP)}:${ClientConfig.DEV_SERVER_PORT}"
     private val httpSchema = if(isProduction) "https" else "http"
