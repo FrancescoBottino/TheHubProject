71464e315e5fe3eb6eef6779549ec4f256bad443
Francesco Bottino <francesco.bottino96@gmail.com>
2025-07-06 17:44:57 +0200

    fix remove https from dev build

--- games/tictactoe/client/build.gradle.kts
+++ games/tictactoe/client/build.gradle.kts
@@ -50,6 +50,7 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(libs.napier)
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.client.logging)
