9f39040f29fb26a20e1ac8863c3535452dbfcae2
Francesco Bottino <francesco.bottino96@gmail.com>
2025-07-07 20:30:31 +0200

    Revert "removed cors"
    This reverts commit df51cdf1b2f18875fd9d1b50967e8522e36d52b7.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.ktor.server.websockets)
     implementation(libs.ktor.server.contentNegotiation)
+    implementation(libs.ktor.server.cors)
     implementation(libs.ktor.server.auth)
     implementation(libs.ktor.server.auth.jwt)
     implementation(libs.ktor.serialization.kotlinx.json.jvm)
